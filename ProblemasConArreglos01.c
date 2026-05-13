cc8bb952a8dc2d0ede1a29817d9657144676808a
Miguel-Reyes-G9 <miguel.alberto.reyes.morales@gmail.com>
2026-05-13 16:37:13 -0600

    Create ProblemasConArreglos01.c

---
+++ ProblemasConArreglos01.c
@@ -0,0 +1,21 @@
+// Escribir un diagrama de flujo tal que, dado como entrada un arreglo unidimensional de enteros, obtenga como resultado la suma de los mismos.
+#include <stdio.h>
+int i, n, cont;
+char c;
+int vect[1000000000000000000000000000000000000000];
+
+void main () {
+  i=0;
+  do {
+    printf ("Dame el valor que quieras agregar a la matriz: \n");
+    scanf ("%d", &n);
+    vect[i]=n;
+    ++i;
+    printf ("Desas agregar otro numero ? S/N \n");
+    scanf (" %c", &c);
+    } while (c=='S' || c=='s');
+  n=0;
+  for (cont=0; cont<=i; cont++)
+    n=n+vect[cont];
+  printf ("La suma de los valores de la matriz es de: %d \n", n);
+}
