1f13b16720d6cf3a2e11d54b178ca17300a328c1
Miguel-Reyes-G9 <miguel.alberto.reyes.morales@gmail.com>
2026-05-12 22:05:07 -0600

    Create ProblemasConCondicionalSI(IF)04.c

---
+++ ProblemasConCondicionalSI(IF)04.c
@@ -0,0 +1,15 @@
+/*Construir un diagrama de flujo que pueda determinar, dados dos números enteros, si un número es divisor del otro.*/
+#include <stdio.h>
+int a, b;
+
+void main () {
+  printf ("Dame tu primer numero: \n");
+  scanf ("%d", &a);
+  printf ("Dame tu segundo numero: \n");
+  scanf ("%d", &b);
+  
+  if (a%b==0) 
+    printf ("El valor %d es un divisor de %d \n", a, b);
+  if (b%a==0) 
+    printf ("El valor %d es un divisor de %d \n", a, b);
+} 
