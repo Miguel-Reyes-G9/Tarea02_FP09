ec7fe7412217b5780c9bbb226c867ef8e45e8ce4
Miguel-Reyes-G9 <miguel.alberto.reyes.morales@gmail.com>
2026-05-12 21:42:28 -0600

    Update ProblemasConCondicionalSI(IF)02.c

--- ProblemasConCondicionalSI(IF)02.c
+++ ProblemasConCondicionalSI(IF)02.c
@@ -1,1 +1,17 @@
-/* */
+/*Dado como dato el sueldo de un trabajador, considere un aumento 
+del 15% si el sueldo es inferior a 1,000 y de un 12% en caso contrario. 
+Imprimir el sueldo con el aumento incorporado. Hacer el diagrama de 
+flujo correspondiente.*/
+#include <stdio.h>
+float s, a;
+
+void main (){
+  printf ("Cual es el sueldo del trabajador: \n");
+  scanf ("%f", &s);
+  if (s<1000)
+    a=(s*0.15);
+  else 
+    a=(s*0.12);
+  s=s+a;
+  printf ("El sueldo considerando el aumento es de: %f \n", s);
+}
