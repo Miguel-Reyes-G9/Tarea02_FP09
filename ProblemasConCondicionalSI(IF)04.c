849fdd64bdb1f7d722270cd788f0019acd4fbbdc
Miguel-Reyes-G9 <miguel.alberto.reyes.morales@gmail.com>
2026-05-12 22:41:26 -0600

    Update ProblemasConCondicionalSI(IF)04.c

--- ProblemasConCondicionalSI(IF)04.c
+++ ProblemasConCondicionalSI(IF)04.c
@@ -8,8 +8,11 @@ void main () {
   printf ("Dame tu segundo numero: \n");
   scanf ("%d", &b);
   
+  if (a%b==0 || b%a==0) {
   if (a%b==0) 
-    printf ("El valor %d es un divisor de %d \n", a, b);
+    printf ("El valor %d es un divisor de %d \n", b, a);
   if (b%a==0) 
-    printf ("El valor %d es un divisor de %d \n", a, b);
+    printf ("El valor %d es un divisor de %d \n", a, b);}
+  else 
+    printf ("De los valores dados, ninguno es divisor del otro \n");
 } 
