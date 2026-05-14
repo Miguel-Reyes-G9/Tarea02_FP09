74168792f2f68bcdc4a32df28642aed5cadf398b
Miguel-Reyes-G9 <miguel.alberto.reyes.morales@gmail.com>
2026-05-13 21:28:46 -0600

    Update ProblemasConArreglos01.c

--- ProblemasConArreglos01.c
+++ ProblemasConArreglos01.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 int i, n, cont;
 char c;
-int vect[1000000000000000000000000000000000000000];
+int vect[100000000];
 
 void main () {
   i=0;
   do {
-    printf ("Dame el valor que quieras agregar a la matriz: \n");
+    printf ("Dame el valor que quieras agregar al arreglo: \n");
     scanf ("%d", &n);
     vect[i]=n;
     ++i;
@@ -17,5 +17,5 @@ void main () {
   n=0;
   for (cont=0; cont<=i; cont++)
     n=n+vect[cont];
-  printf ("La suma de los valores de la matriz es de: %d \n", n);
+  printf ("La suma de los valores del arreglo es de: %d \n", n);
 }
